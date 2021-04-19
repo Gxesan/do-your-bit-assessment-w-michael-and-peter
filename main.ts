d868b15f2d0d19acb872178cb6addf0e48c4fe55
Peter Yoon <78577931+Gxesan@users.noreply.github.com>
2021-04-19 12:07:44 +0800

    Push 1 added temperature and light level sensors

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
-basic.showLeds(`
-    # . . . #
-    . # . # .
-    . . # . .
-    . # . # .
-    # . . . #
-    `)
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    if (input.temperature() > 37.5) {
+        soundExpression.sad.playUntilDone()
+    } else {
+        music.stopAllSounds()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (input.lightLevel() > 50) {
+        music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+    } else {
+        music.stopAllSounds()
+    }
 })
